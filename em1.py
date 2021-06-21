f0cddfeb72b79bdb477611a973fd516be0143fd7
Ruchika Sharan <ruchikaasharan@gmail.com>
2021-06-21 22:03:59 +0530

    Sending Email- alerts created

---
+++ em1.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.message import EmailMessage
+import math, random
+
+
+def generateOTP() :
+    digits = "0123456789"
+    OTP = ""
+    for i in range(6) :
+        OTP += digits[math.floor(random.random() * 10)]
+    return OTP
+    
+
+def email_alert(subject, body, to):
+	msg = EmailMessage()
+	msg.set_content(body)
+	msg['subject']=subject
+	msg['to']=to
+	user = 'ruchika.team@gmail.com'
+	msg['from']=user
+	password = 'ahqlqqaaodwmopfp'
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(user, password)
+	server.send_message(msg)
+	server.quit()
+	
+getotp = generateOTP();
+email_alert('One-time OTP for Inern-Hackathon Project', 'Hey user, your OTP is as folloes: '+getotp, 'sharan6ruchika@gmail.com')
